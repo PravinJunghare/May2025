3c16cd8017f29110790441a8487b71a3548e80c9
PravinJunghare <pravinjungahre01@gmail.com>
2025-07-03 10:39:03 +0530

    No chnages

--- src/main/java/com/qa/opencart/utils/ElementUtil.java
+++ src/main/java/com/qa/opencart/utils/ElementUtil.java
@@ -18,7 +18,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import com.qa.opencart.factory.Driverfactory;
+import com.qa.opencart.factory.DriverFactory;
 
 public class ElementUtil {
 
--- src/test/java/com/qa/opencart/base/BaseTest.java
+++ src/test/java/com/qa/opencart/base/BaseTest.java
@@ -6,13 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
-import com.qa.opencart.factory.Driverfactory;
+import com.qa.opencart.factory.DriverFactory;
+
 import com.qa.opencart.pages.AccountsPage;
 import com.qa.opencart.pages.LoginPage;
 
 public class BaseTest {
 
-	Driverfactory df;
+	DriverFactory df;
 	WebDriver driver;
 	protected Properties prop;
 	
@@ -23,7 +24,7 @@ public class BaseTest {
 
 	@BeforeTest
 	public void setUp() {
-		df = new Driverfactory();// created driver factory object
+		df = new DriverFactory();// created driver factory object
 		prop=df.initProp();
 		//driver = df.initDriver("chrome");// to call initDriver method to get driver
 		driver=df.initDriver(prop);
